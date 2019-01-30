e3cef06e5942dc54e18b53c94be0cd010b15b092
JakePEGG <jake.a.atkin@gmail.com>
2019-01-30 14:12:11 +0000

    altered feature tests to link to our database

--- spec/feature_tests/bookmarks_spec.rb
+++ spec/feature_tests/bookmarks_spec.rb
@@ -1,4 +1,5 @@
 require_relative '/Users/jakeatkin/projects/databases/bookmark/app.rb'
+require 'pg'
 
   feature 'bookmarks' do
     scenario 'expect there to be a bookmark list'do
@@ -9,7 +10,13 @@ end
 
   feature 'viewing a list of bookmarks' do
     scenario 'expect a list of websites' do
-      visit ('/bookmarks')
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.google.com');")
+      connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.destroyallsoftware.com');")
+
+      visit('/bookmarks')
 
       expect(page).to have_content "http://www.makersacademy.com"
       expect(page).to have_content "http://www.google.com"
