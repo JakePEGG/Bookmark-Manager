7ed69ba225b2b4bb1baeb40f3dbae0fcc78b4a9a
JakePEGG <jake.a.atkin@gmail.com>
2019-01-30 16:17:56 +0000

    Added some styling to the website

--- spec/feature_tests/index_spec.rb
+++ spec/feature_tests/index_spec.rb
@@ -3,6 +3,6 @@ require_relative '/Users/jakeatkin/projects/databases/bookmark/app.rb'
 feature "Index" do
   scenario "Expect page to contain Hello world!" do
   visit ('/')
-  expect(page).to have_content("Hello world!")
+  expect(page).to have_content("Bookmark Manager")
 end
 end
